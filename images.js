d4eff7b7dd108d5f23a3b13cd59c98b689f5edb0
ggf-bhilai <ggf.bhilai@gmail.com>
2026-01-24 21:12:13 +0530

    Update images.js

--- images.js
+++ images.js
@@ -50,7 +50,7 @@ const images = [
 "Godrej Yummiez Chicken Nuggets 500gm - Rs. 346.00, Frozen Non-Veg Snacks.jpg",
 "Godrej Yummiez Chilli Garlic Potato Bites 420g - Rs. 125.00, Frozen Veg Snacks.jpg",
 "Godrej Yummiez Veg Burger Patty 340gm - Rs. 131.00, Frozen Veg Snacks.jpg",
-"Golden Crown Beans In Tomato Sauce 450gm - Rs. 100.00, Japanese Thai & Oriental.jpg",
+"Golden Crown Beans In Tomato Sauce 450gm - Rs. 100.00, Canned & Imported Items.jpg",
 "Golden Crown Jalapeno Slices 680gm - Rs. 80.00, Olives Jalapenoes & Canned Products.jpg",
 "Golden Crown MSG - Rs. 100.00, Masale.jpg",
 "Golden Crown Pineapple Slice 850gm - Rs. 180.00, Canned & Imported Items.jpg",
@@ -160,4 +160,5 @@ const images = [
 "Godrej Yummiez Chicken Seekh Kabab 360g - Rs. 275.00, Frozen Non-Veg Snacks.jpg",
 "Veggie Fresh Frozen Green Peas 1kg - Rs. 180.00, Frozen Vegetables.jpg",
 ];
+
 
