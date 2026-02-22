694f9940bae3dd060eb01bd5493d708aba24a33b
ggf-bhilai <ggf.bhilai@gmail.com>
2026-02-22 11:37:24 +0530

    Update images.js

--- images.js
+++ images.js
@@ -1,4 +1,5 @@
 const images = [
+"Fruit Pulp, Sitafal Pulp 1kg - Rs. 300.00, Frozen Fruit Pulp.png",
 "Fruit Pulp, Mango Pulp 1kg - Rs. 300.00, Frozen Fruit Pulp.png",  
 "Disposable, Tea Coffee Cup 100Pcs - Rs. 40.00, Disposable.jpg",
 "Disposable, Tea Coffee Cup 100Pcs - Rs. 60.00, Disposable.jpg",
