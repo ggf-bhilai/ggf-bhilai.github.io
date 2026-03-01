d3a9569388aabeb752659d4c2c24d491459a5ebb
ggf-bhilai <ggf.bhilai@gmail.com>
2026-03-01 11:59:12 +0530

    Update images.js

--- images.js
+++ images.js
@@ -1,4 +1,5 @@
 const images = [
+"Ferrero Rocher, Ferrero Rocher Chocolate 4 Pieces 50g - Rs. 169.00, Chocolate.jpg",  
 "Ferrero Rocher, Ferrero Rocher Chocolate 16 Pieces 200g - Rs. 539.00, Chocolate.jpg",
 "Nutralite, Doodh Shakti Ghee 1Ltr - Rs. 600.00, Dairy.webp",
 "Amul, Amulya Milk Powder 1kg Rs. 500.00, Food Ingredients.jpg",
