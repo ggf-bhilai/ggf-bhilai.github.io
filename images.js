4173ffa441531879a5906012871afc797b02d279
ggf-bhilai <ggf.bhilai@gmail.com>
2026-05-07 12:42:44 +0530

    Fix typo in image filename for Chicken Chilli Sausages

--- images.js
+++ images.js
@@ -185,7 +185,7 @@ const images = [
 "Go, Orange Cheddar Cheese Diced 2kg - Rs. 1400.00, Cheese.jpg",
 "Go, Sandwich Slice 900gm - Rs. 500.00, Cheese.jpg",
 "Go, Double Tonned Milk 1Liter - Rs. 75.00, Dairy.jpg",
-"Godrej Yummiez, Chicken Chilli Sausages 250gm - Rs. 178.00, Frozen Non-Veg Snacks,jpg",
+"Godrej Yummiez, Chicken Chilli Sausages 250gm - Rs. 178.00, Frozen Non-Veg Snacks.jpg",
 "Godrej Yummiez, Chicken & Cheese Nuggets 325g - Rs. 267.00, Frozen Non-Veg Snacks.jpg",
 "Godrej Yummiez, Chicken Garlic Fingers 400g - Rs. 257.00, Frozen Non-Veg Snacks.jpg",
 "Godrej Yummiez, Chicken Nuggets 750g - Rs. 464.00, Frozen Non-Veg Snacks.jpg",
